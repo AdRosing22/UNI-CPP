8193ab64de6f6146daf4c561bde49fc7655ba57a
AdRosing22 <77940488+AdRosing22@users.noreply.github.com>
2021-01-25 00:36:29 +0200

    NOT FINISHED!!
    !!Sparar p.g.a. databyte för att kunna fortsätta

---
+++ ordcount.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
+#include <list>
+using namespace std;
+
+typedef map<string, int> OrdCount_list;
+
+int main()
+{
+
+    string filnamn;
+    cin>>filnamn;
+    ifstream TEXT(filnamn);
+
+    string line, ord;
+    int OrdCount;
+    OrdCount_list OrdCount;
+
+    while(getline(TEXT, line, ' '))
+    {
+        int ignorera;
+
+        while((ignorera = line.find_first_of(".,!?()&:-123456789´")))
+        {
+            line.erase(ignorera, 1);
+        }
+
+        
+        OrdCount++;
+
+
+    }
+    int nuvarande;
+    OrdCount_list::const_iterator nuvarande(OrdCount.begin());
+    
+
+    while(nuvarande!=OrdCount.end())
+    {
+        cout<<nuvarande->first<<" "<<nuvarande->second<<endl;
+    }
+    return 0;
+
+}
